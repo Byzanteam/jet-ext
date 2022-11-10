5476e61a45aa287961dbb65f539780fb1a140688
Phil Chen <06fahchen@gmail.com>
2022-11-10 12:39:56 +0800

    test: add Enum tests

--- test/jet_ext/ecto/enum_test.exs
+++ test/jet_ext/ecto/enum_test.exs
@@ -1,5 +1,28 @@
 defmodule JetExt.Ecto.EnumTest do
   use ExUnit.Case, async: true
 
-  doctest JetExt.Types
+  defmodule EnumType do
+    use JetExt.Ecto.Enum, [:foo, :bar]
+  end
+
+  test "cast/1" do
+    assert EnumType.cast("foo") === {:ok, :foo}
+    assert EnumType.cast("FOO") === {:ok, :foo}
+    assert EnumType.cast(:foo) === {:ok, :foo}
+
+    assert EnumType.cast(:baz) === :error
+  end
+
+  test "load/1" do
+    assert EnumType.load("foo") === {:ok, :foo}
+    assert EnumType.load("FOO") === {:ok, :foo}
+
+    assert EnumType.load("baz") === :error
+  end
+
+  test "dump/1" do
+    assert EnumType.dump(:foo) === {:ok, "FOO"}
+
+    assert EnumType.dump(:baz) === :error
+  end
 end
