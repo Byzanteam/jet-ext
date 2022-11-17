dc9d2e9c0942c807ece4b0cb620a5c86d2caf16d
Phil Chen <06fahchen@gmail.com>
2022-11-17 11:53:07 +0800

    refactor: extract cast/dump/load of the sti builder functions out

--- lib/jet_ext/ecto/sti/builder.ex
+++ lib/jet_ext/ecto/sti/builder.ex
@@ -27,102 +27,17 @@ defmodule JetExt.Ecto.STI.Builder do
 
       @impl Ecto.Type
       def cast(data) when is_map(data) do
-        data
-        |> unquote(intermediate_module).cast()
-        |> case do
-          {:ok, %Ecto.Changeset{valid?: true} = changeset} ->
-            {:ok, Ecto.Changeset.apply_changes(changeset)}
-
-          {:ok, %Ecto.Changeset{valid?: false} = changeset} ->
-            {:error, [validation: :sti, sti_errors: Changeset.collect_errors(changeset)]}
-
-          {:ok, data} when is_struct(data) ->
-            {:ok, data}
-
-          {:error, {:type_absence, field_names}} ->
-            field_names = List.wrap(field_names)
-
-            Logger.log_cast_error(
-              __MODULE__,
-              data,
-              "#{inspect(field_names)} are required"
-            )
-
-            errors = Map.new(field_names, &{&1, [{"can't be blank", validation: :required}]})
-
-            {:error, [validation: :sti, sti_errors: errors]}
-
-          {:error, {:unexpected_type, fields}} ->
-            fields = List.wrap(fields)
-
-            details =
-              Enum.map_join(fields, "\n", fn {field_name, expected_values} ->
-                "#{inspect(field_name)} expected values: #{inspect(expected_values)}"
-              end)
-
-            Logger.log_cast_error(
-              __MODULE__,
-              data,
-              """
-              unexpected type
-              #{inspect(details)}
-              """
-            )
-
-            errors =
-              Map.new(fields, fn {field_name, expected_values} ->
-                {field_name, [{"is invalid", validation: :inclusion, enum: expected_values}]}
-              end)
-
-            {:error, [validation: :sti, sti_errors: errors]}
-        end
-      end
-
-      def cast(data) do
-        Logger.log_cast_error(__MODULE__, data, "expected data is map")
-
-        :error
+        unquote(__MODULE__).__cast__(unquote(intermediate_module), data)
       end
 
       @impl Ecto.Type
       def dump(data) when is_struct(data) do
-        data
-        |> unquote(intermediate_module).dump()
-        |> case do
-          {:ok, result} ->
-            {:ok, result}
-
-          :error ->
-            Logger.log_dump_error(__MODULE__, data)
-            :error
-        end
-      end
-
-      def dump(data) do
-        Logger.log_dump_error(__MODULE__, data)
-
-        :error
+        unquote(__MODULE__).__dump__(unquote(intermediate_module), data)
       end
 
       @impl Ecto.Type
       def load(data) when is_map(data) do
-        data
-        |> unquote(intermediate_module).load()
-        |> case do
-          {:ok, data} ->
-            {:ok, data}
-
-          :error ->
-            Logger.log_load_error(__MODULE__, data)
-
-            :error
-        end
-      end
-
-      def load(data) do
-        Logger.log_load_error(__MODULE__, data)
-
-        :error
+        unquote(__MODULE__).__load__(unquote(intermediate_module), data)
       end
     end
   end
@@ -161,5 +76,108 @@ defmodule JetExt.Ecto.STI.Builder do
         end
       end
     end
+  end
+
+  @doc false
+  @spec __cast__(module(), term()) :: {:ok, term()} | :error | {:error, keyword()}
+  def __cast__(intermediate_module, data) when is_map(data) do
+    data
+    |> intermediate_module.cast()
+    |> case do
+      {:ok, %Ecto.Changeset{valid?: true} = changeset} ->
+        {:ok, Ecto.Changeset.apply_changes(changeset)}
+
+      {:ok, %Ecto.Changeset{valid?: false} = changeset} ->
+        {:error, [validation: :sti, sti_errors: Changeset.collect_errors(changeset)]}
+
+      {:ok, data} when is_struct(data) ->
+        {:ok, data}
+
+      {:error, {:type_absence, field_names}} ->
+        field_names = List.wrap(field_names)
+
+        Logger.log_cast_error(
+          __MODULE__,
+          data,
+          "#{inspect(field_names)} are required"
+        )
+
+        errors = Map.new(field_names, &{&1, [{"can't be blank", validation: :required}]})
+
+        {:error, [validation: :sti, sti_errors: errors]}
+
+      {:error, {:unexpected_type, fields}} ->
+        fields = List.wrap(fields)
+
+        details =
+          Enum.map_join(fields, "\n", fn {field_name, expected_values} ->
+            "#{inspect(field_name)} expected values: #{inspect(expected_values)}"
+          end)
+
+        Logger.log_cast_error(
+          __MODULE__,
+          data,
+          """
+          unexpected type
+          #{inspect(details)}
+          """
+        )
+
+        errors =
+          Map.new(fields, fn {field_name, expected_values} ->
+            {field_name, [{"is invalid", validation: :inclusion, enum: expected_values}]}
+          end)
+
+        {:error, [validation: :sti, sti_errors: errors]}
+    end
+  end
+
+  def __cast__(_intermediate_module, data) do
+    Logger.log_cast_error(__MODULE__, data, "expected data is map")
+
+    :error
+  end
+
+  @doc false
+  @spec __dump__(module(), term()) :: {:ok, term()} | :error
+  def __dump__(intermediate_module, data) when is_struct(data) do
+    data
+    |> intermediate_module.dump()
+    |> case do
+      {:ok, result} ->
+        {:ok, result}
+
+      :error ->
+        Logger.log_dump_error(__MODULE__, data)
+        :error
+    end
+  end
+
+  def __dump__(_intermediate_module, data) do
+    Logger.log_dump_error(__MODULE__, data)
+
+    :error
+  end
+
+  @doc false
+  @spec __load__(module(), term()) :: {:ok, term()} | :error
+  def __load__(intermediate_module, data) when is_map(data) do
+    data
+    |> intermediate_module.load()
+    |> case do
+      {:ok, data} ->
+        {:ok, data}
+
+      :error ->
+        Logger.log_load_error(__MODULE__, data)
+
+        :error
+    end
+  end
+
+  def __load__(_intermediate_module, data) do
+    Logger.log_load_error(__MODULE__, data)
+
+    :error
   end
 end
