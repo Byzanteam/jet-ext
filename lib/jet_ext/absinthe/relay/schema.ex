888bb0d99b22979a847329df9ab150b890175304
Cold Cat <yuchengzhixia@gmail.com>
2023-11-23 16:56:43 +0800

    feat(absinthe): cutsom absinthe relay node id

---
+++ lib/jet_ext/absinthe/relay/schema.ex
@@ -0,0 +1,39 @@
+defmodule JetExt.Absinthe.Relay.Schema do
+  @moduledoc """
+  Used to extend a schema with Relay-specific macros and types.
+
+  See `Absinthe.Relay`.
+  """
+
+  defmacro __using__(flavor) when is_atom(flavor) do
+    do_using(flavor, [])
+  end
+
+  defmacro __using__(opts) when is_list(opts) do
+    opts
+    |> Keyword.get(:flavor, [])
+    |> do_using(opts)
+  end
+
+  defp do_using(flavor, opts) do
+    quote do
+      @pipeline_modifier unquote(__MODULE__)
+      use JetExt.Absinthe.Relay.Schema.Notation, unquote(flavor)
+      import_types Absinthe.Relay.Connection.Types
+
+      @spec __absinthe_relay_global_id_translator__() :: module()
+      def __absinthe_relay_global_id_translator__ do
+        Keyword.get(unquote(opts), :global_id_translator)
+      end
+    end
+  end
+
+  @spec pipeline(Absinthe.Pipeline.t()) :: Absinthe.Pipeline.t()
+  def pipeline(pipeline) do
+    Absinthe.Pipeline.insert_before(
+      pipeline,
+      Absinthe.Phase.Schema.ApplyDeclaration,
+      Absinthe.Relay.Schema.Phase
+    )
+  end
+end
